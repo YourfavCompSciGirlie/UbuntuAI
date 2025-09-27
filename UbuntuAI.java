5a15946dd0fd510ae7d3b0c0793c4fba51cb8c06
Siphamandla Mazibuko <100807070+fawkesdbs@users.noreply.github.com>
2025-09-27 15:34:51 +0200

    Update UbuntuAI.java

--- UbuntuAI.java
+++ UbuntuAI.java
@@ -5,50 +5,107 @@ import dev.robocode.tankroyale.botapi.graphics.Color;
 // ------------------------------------------------------------------
 // UbuntuAI
 // ------------------------------------------------------------------
-// A sample bot original made for Robocode by Mathew Nelson.
-//
-// Probably the first bot you will learn about.
-// Moves in a seesaw motion and spins the gun around at each end.
+// 
 // ------------------------------------------------------------------
 public class UbuntuAI extends Bot {
 
-    // The main method starts our bot
     public static void main(String[] args) {
         new UbuntuAI().start();
     }
 
-    // Called when a new round is started -> initialize and do some movement
     @Override
     public void run() {
+        String operating_mode = "DEF";
+
         setBodyColor(Color.BLACK);
         setTurretColor(Color.BLACK);
-        setRadarColor(Color.BLUE);
-        setScanColor(Color.RED);
+        setRadarColor(Color.GREEN);
+        setScanColor(Color.GREEN);
 
-        // Repeat while the bot is running
+        moveToCorner(100);
+
+        double centerX = getArenaWidth()/2.0;
+        double centerY = getArenaHeight()/2.0;
+        double angleToCenter = bearingTo(centerX, centerY);
+        turnGunRight(angleToCenter);
+
         while (isRunning()) {
-            // Tell the game that when we take move, we'll also want to turn right... a lot
-            setTurnRight(10_000);
-            // Limit our speed to 5
-            setMaxSpeed(3);
-            // Start moving (and turning)
-            forward(5000);
+            operate(operating_mode);
+            turnGunRight(45);
+            turnGunLeft(45);
+
+            int enemies = getEnemyCount();
+            System.out.println(enemies);
+            if (enemies < 5) {
+                setRadarColor(Color.RED);
+                setScanColor(Color.RED);
+                operating_mode = "ATK";
+            } else {
+                setRadarColor(Color.GREEN);
+                setScanColor(Color.GREEN);
+                operating_mode = "DEF";
+            }
         }
     }
 
-    // We saw another bot -> fire!
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        fire(1);
+        double distanceToEnemy = distanceTo(e.getX(), e.getY());
+
+        if (getEnergy() > 70) {
+            if (distanceToEnemy < 200) {
+                // if bot is close and we have high energy, shoot at 3
+                turnRight(directionTo(e.getX(), getY()));
+                forward(100);
+                fire(3);
+            } else {
+                // if bot is far and we have high energy, shoot at 1
+                fire(1);
+            }
+        } else {
+            // if we have low energy, shoot at 0.5
+            fire(0.5);
+        }
     }
 
-    // We were hit by a bullet -> turn perpendicular to the bullet
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
-        // Calculate the bearing to the direction of the bullet
         var bearing = calcBearing(e.getBullet().getDirection());
-
-        // Turn 90 degrees to the bullet direction based on the bearing
         turnRight(90 - bearing);
+    }
+
+    public void operate(String operating_mode) {
+        if (operating_mode == "DEF") {
+            turnRight(directionTo(0, 0));
+            forward(distanceTo(0, 0) - 200);
+            turnRight(directionTo(0, getArenaWidth()));
+        } else if (operating_mode == "ATK") {
+            turnRight(directionTo(0, 0));
+            forward(distanceTo(0, 0) - 200);
+            turnRight(directionTo(0, getArenaWidth()));
+        }
+    }
+
+    public void moveToCorner(double margin) {
+        double[][] corners = {
+            {margin, margin},
+            {margin, getArenaHeight()-margin},
+            {getArenaWidth()-margin, getArenaHeight()-margin},
+            {getArenaWidth()-margin, margin}
+        };
+
+        double[] best = corners[0];
+        double bestDist = distanceTo(best[0], best[1]);
+        for (double[] c : corners) {
+            double d = distanceTo(c[0], c[1]);
+            if (d < bestDist) {
+                bestDist = d;
+                best = c;
+            }
+        }
+
+        double angle = bearingTo(best[0], best[1]);
+        turnRight(angle);
+        forward(bestDist);
     }
 }
