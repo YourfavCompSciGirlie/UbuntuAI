4dac372da31e5aa7f7663c92588e79d0b85ff47f
Siphamandla Mazibuko <s.mazibuko1108@gmail.com>
2025-09-27 14:05:59 +0200

    Corner Strategy

--- UbuntuAI/UbuntuAI.java
+++ UbuntuAI/UbuntuAI.java
@@ -1,3 +1,6 @@
+// import java.awt.geom.*;
+import java.util.*;
+
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 import dev.robocode.tankroyale.botapi.graphics.Color;
@@ -12,43 +15,79 @@ import dev.robocode.tankroyale.botapi.graphics.Color;
 // ------------------------------------------------------------------
 public class UbuntuAI extends Bot {
 
-    // The main method starts our bot
     public static void main(String[] args) {
         new UbuntuAI().start();
     }
 
-    // Called when a new round is started -> initialize and do some movement
     @Override
     public void run() {
+        String operating_mode = "DEF";
+
         setBodyColor(Color.BLACK);
         setTurretColor(Color.BLACK);
-        setRadarColor(Color.BLUE);
-        setScanColor(Color.RED);
+        setRadarColor(Color.GREEN);
+        setScanColor(Color.GREEN);
 
-        // Repeat while the bot is running
         while (isRunning()) {
-            // Tell the game that when we take move, we'll also want to turn right... a lot
+            operate(operating_mode);
             setTurnRight(10_000);
-            // Limit our speed to 5
             setMaxSpeed(3);
-            // Start moving (and turning)
             forward(5000);
+
+            int enemies = getEnemyCount();
+            System.out.println(enemies);
+            if (enemies < 5) {
+                setRadarColor(Color.RED);
+                setScanColor(Color.RED);
+                operating_mode = "ATK";
+            } else {
+                setRadarColor(Color.GREEN);
+                setScanColor(Color.GREEN);
+                operating_mode = "DEF";
+            }
         }
     }
 
-    // We saw another bot -> fire!
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        fire(1);
+        double distanceToEnemy = distanceTo(e.getX(), e.getY());
+
+        if (getEnergy() > 70) {
+            if (distanceToEnemy < 200) {
+                // if bot is close and we have high energy, shoot at 3
+                fire(3);
+            } else {
+                // if bot is far and we have high energy, shoot at 1
+                fire(1);
+            }
+        } else {
+            // if we have low energy, shoot at 0.5
+            fire(0.5);
+        }
     }
 
-    // We were hit by a bullet -> turn perpendicular to the bullet
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
-        // Calculate the bearing to the direction of the bullet
         var bearing = calcBearing(e.getBullet().getDirection());
-
-        // Turn 90 degrees to the bullet direction based on the bearing
         turnRight(90 - bearing);
+    }
+
+    public void operate(String operating_mode) {
+        if (operating_mode == "DEF") {
+            // defend
+        } else if (operating_mode == "ATK") {
+            // attack
+        }
+    }
+
+    public void findCorner() {
+        Map<String, Double> distances = new HashMap<>();
+        distances.put("TL", distanceTo(0, 0));
+        distances.put("BL", distanceTo(0, getArenaHeight()));
+        distances.put("BR", distanceTo(getArenaWidth(), getArenaHeight()));
+        distances.put("TR", distanceTo(getArenaWidth(), 0));
+
+        
+
     }
 }
